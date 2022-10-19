7ef895a8998f4e062593eb69937d0e01dcfcc5f7
kuaqson <qson.ng@gmail.com>
2022-10-19 14:58:32 +0700

    Limit

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,6 +3,6 @@ import { IPaginatorRequest } from 'src/interfaces/common';
 export const APP_NAME = 'simon_cafe';
 export const DEFAULT_PAGINATOR: IPaginatorRequest = {
   offset: 0,
-  limit: 5,
+  limit: 30,
   q: undefined,
 };
